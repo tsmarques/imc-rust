5f27ff0f9c08cc8a0b6361c37d7a0af47f545a58
Tiago Sá Marques <samarques.tiago@gmail.com>
2019-11-23 15:58:01 +0000

    lib: Add test to log imc messages to temporary file

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,30 @@ pub mod imc;
 
 #[cfg(test)]
 mod tests {
+    use std::env;
     use std::fs::File;
     use std::io::Write;
+    use crate::imc::*;
+    use crate::imc::Heartbeat::Heartbeat;
+    use crate::imc::LoggingControl::{LoggingControl, ControlOperationEnum};
+    use std::path::PathBuf;
+    use crate::imc::Message::Message;
+    use bytes::{BufMut, BytesMut};
 
     #[test]
     fn all_messages() {
-        let mut file = File::create("/tmp/rust.lsf");
+        let temp_directory: PathBuf = env::temp_dir();
+        let file_path = temp_directory.join("rust.lsf");
+        let mut file = File::create(file_path).unwrap();
+
+        let mut bfr :bytes::BytesMut = BytesMut::with_capacity(4096);;
+
+        let mut log_control:LoggingControl = LoggingControl::new();
+        log_control.name = "rust-test";
+        log_control.op = ControlOperationEnum::COP_REQUEST_START;
+
+        log_control.serialize(&mut bfr);
+//        writeln!(&mut file, bfr).unwrap();
+        file.write_all(bfr.as_ref()).unwrap();
     }
 }
