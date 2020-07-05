0796a06e5a8bc94be7ea5530f8fca470ae650020
Tiago Sá Marques <samarques.tiago@gmail.com>
2020-07-05 22:24:14 +0100

    imc: add newly generated IMC messages

--- src/imc/LoggingControl.rs
+++ src/imc/LoggingControl.rs
@@ -1,58 +1,62 @@
 use crate::imc::Message::*;
-use crate::imc::{IMC_CONST_UNK_EID, DUNE_IMC_CONST_SYNC};
+use crate::imc::{DUNE_IMC_CONST_SYNC, IMC_CONST_UNK_EID};
 
-use bytes::BufMut;
 use crate::imc::Header::Header;
+use bytes::BufMut;
 
-const c_msg_id :u16 = 102;
+// This operation instructs the logging manager to send a
+// message with operation CURRENT_NAME containing the complete
+// name of the current log in the field 'name'. The field
+// 'name' with this operation type has no meaning.
 
-pub enum ControlOperationEnum
-{
-    // Request Start of Logging.
+const c_msg_id: u16 = 102;
+
+pub enum op {
+    // Request Start of Logging
     COP_REQUEST_START = 0,
-    // Logging Started.
+    // Logging Started
     COP_STARTED = 1,
-    // Request Logging Stop.
+    // Request Logging Stop
     COP_REQUEST_STOP = 2,
-    // Logging Stopped.
+    // Logging Stopped
     COP_STOPPED = 3,
-    // Request Current Log Name.
+    // Request Current Log Name
     COP_REQUEST_CURRENT_NAME = 4,
-    // Current Log Name.
-    COP_CURRENT_NAME = 5
+    // Current Log Name
+    COP_CURRENT_NAME = 5,
 }
 
-impl ControlOperationEnum
-{
-    pub fn as_u8(&self) -> u8
-    {
+impl op {
+    pub fn as_u8(&self) -> u8 {
         match self {
-            ControlOperationEnum::COP_REQUEST_START =>        0,
-            ControlOperationEnum::COP_STARTED =>              1,
-            ControlOperationEnum::COP_REQUEST_STOP =>         2,
-            ControlOperationEnum::COP_STOPPED =>              3,
-            ControlOperationEnum::COP_REQUEST_CURRENT_NAME => 4,
-            ControlOperationEnum::COP_CURRENT_NAME =>         5,
+            COP_REQUEST_START => 0,
+            COP_STARTED => 1,
+            COP_REQUEST_STOP => 2,
+            COP_STOPPED => 3,
+            COP_REQUEST_CURRENT_NAME => 4,
+            COP_CURRENT_NAME => 5,
         }
     }
 }
 
-
-pub(crate) struct LoggingControl
-{
+pub struct LoggingControl {
     pub header: Header,
-    pub op: ControlOperationEnum,
-    pub name: &'static str,
+    // The logging manager will send a message with this operation
+    // when asked via the REQUEST_CURRENT_NAME operation. The field
+    // 'name' contains the complete name of the log.
+    pub op: u8,
+
+    // The meaning of this field depends on the operation and is
+    // explained in the operation's description.
+    pub name: String,
 }
 
-impl LoggingControl
-{
-    pub(crate) fn new() -> LoggingControl
-    {
+impl LoggingControl {
+    pub fn new() -> LoggingControl {
         let mut msg = LoggingControl {
-            header : Header::new(c_msg_id),
-            op: ControlOperationEnum::COP_REQUEST_START,
-            name: ""
+            header: Header::new(c_msg_id),
+            op: 0,
+            name: String::new(),
         };
 
         msg.set_size(msg.payload_serialization_size() as u16);
@@ -61,37 +65,28 @@ impl LoggingControl
     }
 }
 
-impl Message for LoggingControl
-{
+impl Message for LoggingControl {
     fn get_header(&mut self) -> &mut Header {
         &mut self.header
     }
 
-    fn static_id(&self) -> u16
-    {
+    fn static_id(&self) -> u16 {
         c_msg_id
     }
 
-    fn clear(&mut self)
-    {
-        self.name = "";
-        self.op = ControlOperationEnum::COP_REQUEST_START;
+    fn clear(&mut self) {}
+
+    fn fixed_serialization_size(&self) -> usize {
+        0
     }
 
-    fn fixed_serialization_size(&self) -> usize
-    {
-        1
+    fn dynamic_serialization_size(&self) -> usize {
+        unimplemented!();
     }
 
-    fn dynamic_serialization_size(&self) -> usize
-    {
-        self.name.len() + 2
-    }
-
-    fn serialize(&self, bfr :&mut bytes::BytesMut)
-    {
+    fn serialize(&self, bfr: &mut bytes::BytesMut) {
         self.header.serialize(bfr);
-        bfr.put(self.op.as_u8());
+        bfr.put_u8(self.op);
         serialize_string!(bfr, self.name);
         serialize_footer(bfr);
     }
