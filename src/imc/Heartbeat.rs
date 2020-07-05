f7334eb3a53fe588f2449ad9a016e887ec393f25
Tiago Sá Marques <samarques.tiago@gmail.com>
2020-07-05 22:27:13 +0100

    imc: add new generated Header

--- src/imc/Heartbeat.rs
+++ src/imc/Heartbeat.rs
@@ -1,64 +1,53 @@
-use crate::imc::Message::{Message, serialize_footer};
-use crate::imc::Header::Header;
-use crate::imc::{IMC_CONST_UNK_EID, DUNE_IMC_CONST_SYNC};
+use crate::imc::Message::*;
+use crate::imc::{DUNE_IMC_CONST_SYNC, IMC_CONST_UNK_EID};
 
+use crate::imc::Header::Header;
 use bytes::BufMut;
 
-const c_msg_id :u16 = 150;
+// The Heartbeat message is used to inform other modules that the
+// sending entity's system is running normally and communications
+// are alive.
 
-pub struct Heartbeat
-{
-    header: Header,
+const c_msg_id: u16 = 150;
+
+pub struct Heartbeat {
+    pub header: Header,
 }
 
-impl Heartbeat
-{
-    fn new() -> Heartbeat
-    {
-        let msg = Heartbeat{
-            header :Header{
-                sync: DUNE_IMC_CONST_SYNC,
-                mgid: c_msg_id,
-                size: 0,
-                timestamp: 0.0,
-                src: 0xffff,
-                src_ent: IMC_CONST_UNK_EID,
-                dst: 0xffff,
-                dst_ent: IMC_CONST_UNK_EID
-            }
+impl Heartbeat {
+    pub fn new() -> Heartbeat {
+        let mut msg = Heartbeat {
+            header: Header::new(c_msg_id),
         };
+
+        msg.set_size(msg.payload_serialization_size() as u16);
 
         msg
     }
 }
 
-impl Message for Heartbeat
-{
+impl Message for Heartbeat {
     fn get_header(&mut self) -> &mut Header {
         &mut self.header
     }
 
-    fn static_id(&self) -> u16
-    {
+    fn static_id(&self) -> u16 {
         c_msg_id
     }
 
-    fn clear(&mut self)
-    { }
+    fn clear(&mut self) {}
 
-    fn fixed_serialization_size(&self) -> usize
-    {
+    fn fixed_serialization_size(&self) -> usize {
         0
     }
 
-    fn dynamic_serialization_size(&self) -> usize
-    {
-        0
+    fn dynamic_serialization_size(&self) -> usize {
+        unimplemented!();
     }
 
-    fn serialize(&self, bfr :&mut bytes::BytesMut)
-    {
+    fn serialize(&self, bfr: &mut bytes::BytesMut) {
         self.header.serialize(bfr);
+
         serialize_footer(bfr);
     }
 }
