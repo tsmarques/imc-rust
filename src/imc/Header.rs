97595da02fe761a430c26ca03dfee165f5252de0
Tiago Sá Marques <samarques.tiago@gmail.com>
2020-07-07 16:38:13 +0100

    imc: Forgot to commit new Header format

--- src/imc/Header.rs
+++ src/imc/Header.rs
@@ -14,60 +14,78 @@ pub struct Header {
     /// The packet recipient is responsible for the correct
     /// interpretation of the synchronization number and byte order
     /// conversions.
-    pub sync: u16,
+    pub _sync: u16,
 
     /// The identification number of the message contained in the
     /// packet. This field is used for correct message interpretation
     /// and deserialization.
-    pub mgid: u16,
+    pub _mgid: u16,
 
     /// The size of the message data in the packet.
-    pub size: u16,
+    pub _size: u16,
 
     /// The time when the packet was sent, as seen by the packet
     /// dispatcher. The number of seconds is represented in Universal
     /// Coordinated Time (UCT) in seconds since Jan 1, 1970 using IEEE
     /// double precision floating point numbers.
-    pub timestamp: f64,
+    pub _timestamp: f64,
 
     /// The Source IMC system ID.
-    pub src: u16,
+    pub _src: u16,
 
     /// The entity generating this message at the source address.
-    pub src_ent: u8,
+    pub _src_ent: u8,
 
     /// The Destination IMC system ID.
-    pub dst: u16,
+    pub _dst: u16,
 
     /// The entity that should process this message at the destination
     /// address.
-    pub dst_ent: u8,
+    pub _dst_ent: u8,
 }
 
 impl Header {
     pub(crate) fn new(msg_id: u16) -> Header {
         let mut header = Header {
-            sync: 0xFE54,
-            mgid: 0,
-            size: 0,
-            timestamp: 0.0,
-            src: 0,
-            src_ent: 0,
-            dst: 0,
-            dst_ent: 0,
+            _sync: 0xFE54,
+            _mgid: Default::default(),
+            _size: Default::default(),
+            _timestamp: Default::default(),
+            _src: Default::default(),
+            _src_ent: Default::default(),
+            _dst: Default::default(),
+            _dst_ent: Default::default(),
         };
 
         header
     }
 
+    pub fn clear(&mut self) {
+        self._sync = Default::default();
+
+        self._mgid = Default::default();
+
+        self._size = Default::default();
+
+        self._timestamp = Default::default();
+
+        self._src = Default::default();
+
+        self._src_ent = Default::default();
+
+        self._dst = Default::default();
+
+        self._dst_ent = Default::default();
+    }
+
     pub fn serialize(&self, bfr: &mut bytes::BytesMut) {
-        bfr.put_u16_le(self.sync);
-        bfr.put_u16_le(self.mgid);
-        bfr.put_u16_le(self.size);
-        bfr.put_f64_le(self.timestamp);
-        bfr.put_u16_le(self.src);
-        bfr.put_u8(self.src_ent);
-        bfr.put_u16_le(self.dst);
-        bfr.put_u8(self.dst_ent);
+        bfr.put_u16_le(self._sync);
+        bfr.put_u16_le(self._mgid);
+        bfr.put_u16_le(self._size);
+        bfr.put_f64_le(self._timestamp);
+        bfr.put_u16_le(self._src);
+        bfr.put_u8(self._src_ent);
+        bfr.put_u16_le(self._dst);
+        bfr.put_u8(self._dst_ent);
     }
 }
