bcd73a96f6a1b56ffa09dd895f5121ccf9363893
Tiago Sá Marques <samarques.tiago@gmail.com>
2020-07-20 13:15:41 +0100

    lib: preparation for unit testing of bindings

---
+++ src/imc/tests/mod.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_bla() {
+        assert_eq!(1, 1);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,3 @@
 #[allow(non_upper_case_globals)]
 #[allow(non_snake_case)]
 pub mod imc;
-
-#[cfg(test)]
-mod tests {
-    use crate::imc::Heartbeat::Heartbeat;
-    use crate::imc::LoggingControl::{ControlOperationEnum, LoggingControl};
-    use crate::imc::Message::Message;
-    use crate::imc::*;
-    use bytes::{BufMut, BytesMut};
-    use std::env;
-    use std::fs::File;
-    use std::io::Write;
-    use std::path::PathBuf;
-
-    fn log_write(bfr: &mut bytes::BytesMut, file: &mut File, msg: &mut dyn Message) {
-        msg.set_timestamp_secs(utils::get_timestamp_secs());
-        msg.serialize(bfr);
-    }
-
-    #[test]
-    fn all_messages() {
-        let temp_directory: PathBuf = env::temp_dir();
-        let file_path = temp_directory.join("rust.lsf");
-        let mut file = File::create(file_path).unwrap();
-
-        let mut bfr: bytes::BytesMut = BytesMut::with_capacity(4096);
-
-        let mut log_control: LoggingControl = LoggingControl::new();
-        log_control.name = "rust-test";
-        log_control.op = ControlOperationEnum::COP_REQUEST_START;
-
-        file.write_all(bfr.as_ref()).unwrap();
-    }
-}
