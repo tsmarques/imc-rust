0fece0a6e4f83ca70a655f1712abd9246930a489
Tiago Sá Marques <tmarques@oceanscan-mst.com>
2021-10-19 23:37:09 +0100

    Update copyright

--- src/Magnetometer.rs
+++ src/Magnetometer.rs
@@ -1,5 +1,5 @@
 //###########################################################################
-// Copyright 2017 OceanScan - Marine Systems & Technology, Lda.             #
+// Copyright 2021 OceanScan - Marine Systems & Technology, Lda.             #
 //###########################################################################
 // Licensed under the Apache License, Version 2.0 (the "License");          #
 // you may not use this file except in compliance with the License.         #
